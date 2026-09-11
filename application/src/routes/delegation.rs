85d9b2b465fe76ce4434fc637f1636c35fc560e2
dvernon-tacc <121819040+dvernon-tacc@users.noreply.github.com>
2026-09-11 13:14:41 -0500

    Dvernon/delegation fix (#6)
    * More cleanup of delegations, and adding delete
    
    * Adding deployment files

--- application/src/routes/delegation.rs
+++ application/src/routes/delegation.rs
@@ -1,46 +1,70 @@
-use axum::extract::State;
+use axum::extract::{Path, Query, State};
 use axum::{Json, Router};
-use axum::routing::{get, post};
+use axum::routing::{delete, get, post};
 use http::StatusCode;
 use serde::Deserialize;
 use crate::AppState;
 use crate::routes::api_obj_model::delegations::Delegation;
 use crate::routes::api_obj_model::tms_response::TmsResponse;
-use crate::services::delegation_service::{add_delegation, get_delegations};
+use crate::services::delegation_service::{add_delegation, delete_delegation, get_delegations};
 use crate::utils::app_error::AppError;
 use crate::utils::jwt_utils::JwtValidator;
 
 // params for tokens request
 #[derive(Debug, Deserialize)]
 pub struct AddDelegationRequest {
+    client_name : String,
     resource_provider_id: String,
     resource_provider_account: String,
 }
-
+#[derive(Debug, Deserialize)]
+pub struct GetDelegationQueryParams {
+    client_name : Option<String>,
+}
 pub async fn router() -> Router<AppState> {
     Router::new()
         .route("/delegations", post(add_delegation_handler))
-        .route("/delegations", get(get_delegations_handler))
+        .route("/delegations/", get(get_delegations_handler))
+        .route("/delegations/{client_name}", get(get_delegations_handler_with_client))
+        .route("/delegations/{client_name}/{delegation_id}", delete(delete_delegations_handler))
 }
 #[axum::debug_handler]
 pub async fn add_delegation_handler(State(app_state): State<AppState>,
                                     JwtValidator(security_context): JwtValidator,
                                     Json(add_delegation_request): Json<AddDelegationRequest>,
                                     ) -> anyhow::Result<TmsResponse<Delegation>, AppError> {
-    let delegation = add_delegation(&app_state.db_pool, &security_context.tms_identity,
-        &security_context.client_id, &add_delegation_request.resource_provider_id,
+    let delegation = add_delegation(&app_state.db_pool, &security_context,
+        &add_delegation_request.client_name, &add_delegation_request.resource_provider_id,
         &add_delegation_request.resource_provider_account).await?;
     Ok(TmsResponse::builder(StatusCode::OK).entity(delegation.into()).build())
 }
 #[axum::debug_handler]
-pub async fn get_delegations_handler(State(app_state): State<AppState>,
-                                    JwtValidator(security_context): JwtValidator,
+pub async fn get_delegations_handler_with_client(State(app_state): State<AppState>,
+                                     Path(client_name):Path<String>,
+                                     JwtValidator(security_context): JwtValidator,
 ) -> anyhow::Result<TmsResponse<Vec<Delegation>>, AppError> {
-    let delegations = get_delegations(&app_state.db_pool, &security_context.tms_identity,
-                                    &security_context.client_id).await?;
+    let delegations = get_delegations(&app_state.db_pool, &security_context, &Some(client_name)).await?;
     let mut result_delegations = Vec::with_capacity(delegations.len());
     for delegation in delegations {
         result_delegations.push(delegation.into());
     }
     Ok(TmsResponse::builder(StatusCode::OK).entity(result_delegations).build())
+}
+#[axum::debug_handler]
+pub async fn get_delegations_handler(State(app_state): State<AppState>,
+                                                 JwtValidator(security_context): JwtValidator,
+) -> anyhow::Result<TmsResponse<Vec<Delegation>>, AppError> {
+    let delegations = get_delegations(&app_state.db_pool, &security_context, &None).await?;
+    let mut result_delegations = Vec::with_capacity(delegations.len());
+    for delegation in delegations {
+        result_delegations.push(delegation.into());
+    }
+    Ok(TmsResponse::builder(StatusCode::OK).entity(result_delegations).build())
+}
+#[axum::debug_handler]
+pub async fn delete_delegations_handler(State(app_state): State<AppState>, Path((client_name, delegation_id)): Path<(String, i32)>,
+                                     JwtValidator(security_context): JwtValidator,
+) -> anyhow::Result<TmsResponse<Delegation>, AppError> {
+    let delegation = delete_delegation(&app_state.db_pool, &security_context, &client_name, delegation_id).await?;
+    Ok(TmsResponse::builder(StatusCode::OK).entity(delegation.into()).build())
 }
--- application/src/services/delegation_service.rs
+++ application/src/services/delegation_service.rs
@@ -1,15 +1,18 @@
 use chrono::Utc;
 use sqlx::{PgPool, PgTransaction};
 use chrono::Duration;
-use tms_lib::utils::service_error::ServiceError::NotFound;
-use crate::db::delegations_dao::{db_get_delegations, db_insert_delegation};
+use tms_lib::utils::service_error::ServiceError::{BadRequest, NotFound};
+use crate::db::client_dao::{db_get_client_by_id, db_get_client_by_name};
+use crate::db::delegations_dao::{db_delete_delegation, db_get_delegations, db_insert_delegation};
 use crate::db::resource_provider_logins_dao::{db_get_resource_provider_login};
 use crate::obj_model::delegation::Delegation;
 use crate::utils::configuration::Configuration;
+use crate::utils::jwt_utils::{token_matches_client, SecurityContext};
 
 pub async fn add_delegation(
-    db_pool: &PgPool, tms_identity:&String, client_id:&String, rp_id:&String, rp_account:&String
+    db_pool: &PgPool, security_context: &SecurityContext, client_name:&String, rp_id:&String, rp_account:&String
 ) -> anyhow::Result<Delegation> {
+    let tms_identity = &security_context.tms_identity;
     let configuration = Configuration::get(&db_pool).await?;
     let expiration = &configuration.delegation_policy_config.get_delegation_expiration()?;
     let mut tx = db_pool.begin().await?;
@@ -20,9 +23,16 @@ pub async fn add_delegation(
         Err(e) => return Err(e.into()),
     };
 
-    let delegation = db_insert_delegation(&mut tx, client_id, rp_account,
-                                          &expiration,
-                                          tms_identity, rp_id).await?;
+    let client = db_get_client_by_name(&mut tx, &client_name).await?;
+    let client_id = client.client_id;
+    if ! token_matches_client(security_context, &client_id) {
+        return Err(BadRequest(
+            format!("Token client '{}' is not allowed to delegate for client id: '{}' name: '{}'",
+                    security_context.client_id, &client_id, client_name)).into());
+    }
+
+    let delegation = db_insert_delegation(&mut tx, &client_id, rp_account,
+                                          &expiration, tms_identity, rp_id).await?;
     tx.commit().await?;
     Ok(delegation)
 }
@@ -40,10 +50,61 @@ async fn ensure_recent_rp_login<'a>(tx: &mut PgTransaction<'a>, tms_identity:&St
 }
 
 pub async fn get_delegations(
-    db_pool: &PgPool, tms_identity:&String, client_id:&String) -> anyhow::Result<Vec<Delegation>> {
+    db_pool: &PgPool, security_context: &SecurityContext, client_name:&Option<String>) -> anyhow::Result<Vec<Delegation>> {
     let mut tx = db_pool.begin().await?;
+    let mut client_id_for_delegations = None;
 
-    let delegations = db_get_delegations(&mut tx, client_id, tms_identity).await?;
+    match client_name {
+        // must be tms client to get all delegations - if no client id is specified, and the token is for
+        // a client other than the tms portal client, set the client id to what's in the token
+        None => {
+            if ! security_context.is_tms_client {
+                let client = db_get_client_by_id(&mut tx, &security_context.client_id).await?;
+                client_id_for_delegations = Some(client.client_id)
+            }
+        }
+
+        // Since we have a client name, it must match what's in the token or the token must be for the
+        // tms portal (which is allowed to view all delegations for a user)
+        Some(client_name) => {
+            let client = db_get_client_by_name(&mut tx, &client_name).await?;
+            let client_id = client.client_id;
+
+            if ! token_matches_client(security_context, &client_id) {
+                return Err(BadRequest(
+                    format!("Token client '{}' is not allowed to delegate for client id: '{}' name: '{}'",
+                            &security_context.client_id, &client_id, &client_name)).into());
+            }
+        }
+    }
+
+    let delegations = db_get_delegations(&mut tx, &client_id_for_delegations,
+                                         &security_context.tms_identity).await?;
     tx.commit().await?;
     Ok(delegations)
+}
+pub async fn delete_delegation(
+    db_pool: &PgPool, security_context: &SecurityContext, client_name:&String, delegation_id:i32) -> anyhow::Result<Delegation> {
+    let tms_identity = &security_context.tms_identity;
+    // let configuration = Configuration::get(&db_pool).await?;
+    let mut tx = db_pool.begin().await?;
+
+    // I feel like we don't need a recent auth for this ...  but I could be wrong
+    // // First check that the rp_id/rp_account are linked, enabled, and not expired
+    // let _ = match ensure_recent_rp_login(&mut tx, tms_identity, rp_account, rp_id, configuration.delegation_policy_config.delegation_max_mins_since_login).await {
+    //     Ok(recent_login) => Some(recent_login),
+    //     Err(e) => return Err(e.into()),
+    // };
+
+    let client = db_get_client_by_name(&mut tx, &client_name).await?;
+    let client_id = client.client_id;
+    if ! token_matches_client(security_context, &client_id) {
+        return Err(BadRequest(
+            format!("Token client '{}' is not allowed to delegate for client id: '{}' name: '{}'",
+                    security_context.client_id, &client_id, client_name)).into());
+    }
+
+    let delegation = db_delete_delegation(&mut tx, tms_identity, &client_id, delegation_id).await?;
+    tx.commit().await?;
+    Ok(delegation)
 }
